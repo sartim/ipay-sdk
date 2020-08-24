c0d06365cb84131d328f436faf5e984ee7ef74e8
Tim <sarrtim@gmail.com>
2020-08-24 15:37:24 +0300

    Add config.py with url constants

---
+++ ipay_sdk/config.py
@@ -0,0 +1,4 @@
+class BaseConfig:
+    CARD_PAYMENT_URL = "https://apis.ipayafrica.com/payments/v2/transact/cc"
+    PAYMENT_STATUS_URL = "https://apis.ipayafrica.com/payments/v2/transaction/search"
+    PAYMENT_REFUND_URL = "https://apis.ipayafrica.com/payments/v2/transaction/refund"
