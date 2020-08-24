391ff0ad767f72639f9c77a0df2c174b182642a8
Tim <sarrtim@gmail.com>
2020-08-24 15:39:40 +0300

    Add initiator url constant

--- ipay_sdk/config.py
+++ ipay_sdk/config.py
@@ -1,4 +1,5 @@
 class BaseConfig:
+    INITIATOR_URL = "https://apis.ipayafrica.com/payments/v2/transact"
     CARD_PAYMENT_URL = "https://apis.ipayafrica.com/payments/v2/transact/cc"
     PAYMENT_STATUS_URL = "https://apis.ipayafrica.com/payments/v2/transaction/search"
     PAYMENT_REFUND_URL = "https://apis.ipayafrica.com/payments/v2/transaction/refund"
