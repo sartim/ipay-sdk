77b12d8d4133d481c2d2edebf2a3cb16b9e03435
Tim <sarrtim@gmail.com>
2020-08-24 12:19:55 +0300

    Init

---
+++ README.md
@@ -0,0 +1,22 @@
+# IPAY SDK
+
+[![Language](https://img.shields.io/badge/language-python-green.svg)](https://github.com/sartim/ipay-sdk)
+
+Unofficial python ipay sdk for python devs.
+
+
+***Installation***
+
+    $ pip install ipay-sdk
+
+***Running tests***
+
+    $ pip install pytest
+    $ pytest
+ 
+To test for different python environments use
+    
+    $ pip install tox
+    $ tox
+ 
+Read more on [IPAY API](https://dev.ipayafrica.com/).
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ipay_sdk',
+    version='0.0.1',
+    description='Ipay SDK',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/sartim/ipay-sdk',
+    author='write2sartim@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'python-dotenv',
+        'requests',
+    ],
+    zip_safe=False,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
