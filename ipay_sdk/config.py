7842206c6c865ad644070c11f836dca97ab34c87
Tim <sarrtim@gmail.com>
2020-08-31 16:01:56 +0300

    Add generate checkout url constant

--- ipay_sdk/config.py
+++ ipay_sdk/config.py
@@ -1,4 +1,5 @@
 class BaseConfig:
+    GENERATE_CHECKOUT_URL = "https://payments.ipayafrica.com/v3/ke"
     INITIATOR_URL = "https://apis.ipayafrica.com/payments/v2/transact"
     CARD_PAYMENT_URL = "https://apis.ipayafrica.com/payments/v2/transact/cc"
     PAYMENT_STATUS_URL = "https://apis.ipayafrica.com/payments/v2/transaction/search"
